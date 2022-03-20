32b4cd329a6cc9da0481a1a3f2ae6e4c9b20f953
Daniel <daniel.vahosmendoza@telecom-paris.fr>
2022-03-20 10:45:09 +0100

    working still in new_solid

--- tp-led-matrix/src/image.rs
+++ tp-led-matrix/src/image.rs
@@ -1,8 +1,11 @@
-pub use gamma::*;
-use micromath::F32Ext;
-use std::ops::Mul;
+
 
 pub mod image {
+use crate::gamma::gamma;
+use micromath::F32Ext;
+use core::ops::Mul;
+use core::ops::Div;
+
 #[derive(Default, Copy, Clone)]
 //Create three unsigned bytes for primary colors
 pub struct Color{
@@ -35,30 +38,62 @@ impl Color{
     pub fn gamma_correct(&self) -> Self{
         Color{
             r: gamma:: gamma_correct(self.r),
-            g: gamma:: gamma_correct(self,g),
-            b: gamma:: gamma_correct(self,b),
+            g: gamma:: gamma_correct(self.g),
+            b: gamma:: gamma_correct(self.b),
+        }
+    }
+    }
+    impl Mul<f32> for Color{
+    //pub trait Mul<Rhs = Self> {
+        type Output= Color;
+        fn mul(self, rhs: f32) -> Self::Output{
+            let r = ((self.r as f32)* rhs) as u8;
+            let g = ((self.g as f32)*rhs) as u8;
+            let b = ((self.b as f32) *rhs) as u8;
+            Color{r,g,b}
         }
     }
 
-    pub trait Mul<Rhs = Self> {
-        type Output;
-        fn mul(self, rhs: Rhs) -> Self::Output{
-            r = self.r * rhs.r;
-            g = self.g * rhs.g;
-            b = self.b * rhs.b;
-            Self::new(r,g,b)
+    impl Div<f32> for Color{
+        type Output= Color;
+        fn div(self, rhs: f32) -> Self::Output{
+            let r = ((self.r as f32)/rhs) as u8;
+            let g = ((self.g as f32)/rhs) as u8;
+            let b = ((self.b as f32)/rhs) as u8;
+            Color{r,g,b}
         }
     }
 
-    pub trait Div<Rhs = Self> {
-        type Output;
-        fn div(self, rhs: Rhs) -> Self::Output{
-            r = self.r /rhs.r;
-            g = self.g / rhs.g;
-            b = self.b / rhs.b;
-            Self::new(r,g,b)
-        }
-    }
+    pub struct Image([Color; 64]);
 
+    impl Image{
+        pub fn new_solid(color: Color) -> Self{
+
+            let mut ima: Image;
+            for i in 0..64{
+                ima.0[i]= color;
+            }
+            return ima;
+        }
+        /*
+        pub trait Default {
+            fn default(color: RED) -> Self{
+                let mut ima: Image;
+                for i in 0..64{
+                    ima.0[i]= color;
+                }
+            }
+        }
+
+        pub trait Index<Idx: Color> {
+            type Output: Color;
+            fn index(&self, index: Idx) -> &Self::Output{
+            }
+        }
+
+        pub trait IndexMut<Idx: ?Sized>: Index<Idx> {
+            fn index_mut(&mut self, index: Idx) -> &mut Self::Output;
+        }
+        */
     }
 }
