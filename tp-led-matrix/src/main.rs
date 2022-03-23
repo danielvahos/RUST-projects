896c5a7d325ecf3b5b2bf08973d7b3cb2fe617c5
Daniel <daniel.vahosmendoza@telecom-paris.fr>
2022-03-23 17:10:02 +0100

    displaying something finished!

--- tp-led-matrix/src/main.rs
+++ tp-led-matrix/src/main.rs
@@ -1,15 +1,22 @@
 #![no_std]
 #![no_main]
+use rtt_target::rtt_init_print;
+use rtt_target::rprintln;
 
 use cortex_m_rt::entry;
 use stm32l4 as _;   // Just to link it in the executable (it provides the vector table)
 
+/* 
 #[panic_handler]
 fn panic_handler(_panic_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
+*/
+use panic_rtt_target as _;
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Hello, world!");
     panic!("The program stopped");
-}
+ }
