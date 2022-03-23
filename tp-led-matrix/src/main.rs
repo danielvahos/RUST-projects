0551a4fa3c08a4fe01fc110cfa0302c21cdf293d
Daniel <daniel.vahosmendoza@telecom-paris.fr>
2022-03-23 15:47:38 +0100

    main program

---
+++ tp-led-matrix/src/main.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use stm32l4 as _;   // Just to link it in the executable (it provides the vector table)
+
+#[panic_handler]
+fn panic_handler(_panic_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[entry]
+fn main() -> ! {
+    panic!("The program stopped");
+}
