dcd20e8cf1748c270795972567975171fd53b9b3
Daniel <daniel.vahosmendoza@telecom-paris.fr>
2022-04-05 17:08:11 +0200

    Configuring the hardware well done

--- tp-led-matrix/src/main.rs
+++ tp-led-matrix/src/main.rs
@@ -9,6 +9,7 @@ use stm32l4 as _;   // Just to link it in the executable (it provides the vector
 use panic_probe as _;
 use defmt_rtt as _;
 
+use stm32l4xx_hal::{pac, prelude::*};
 
 
 /*
@@ -18,7 +19,7 @@ fn panic_handler(_panic_info: &core::panic::PanicInfo) -> ! {
 }
 */
 //use panic_rtt_target as _;
-
+/* 
 #[entry]
 fn main() -> ! {
     //rtt_init_print!();
@@ -26,3 +27,26 @@ fn main() -> ! {
     //rprintln!("Hello, world!");
     panic!("The program stopped");
  }
+*/
+
+ #[entry]
+fn main() -> ! {
+    let cp = pac::CorePeripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    run(cp, dp)
+}
+
+fn run(_cp: pac::CorePeripherals, dp: pac::Peripherals) -> ! {
+    // Get high-level representations of hardware modules
+    let mut rcc = dp.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
+    let mut pwr = dp.PWR.constrain(&mut rcc.apb1r1);
+
+    // Setup the clocks at 80MHz using HSI (by default since HSE/MSI are not configured).
+    // The flash wait states will be configured accordingly.
+    let clocks = rcc.cfgr.sysclk(80.MHz()).freeze(&mut flash.acr, &mut pwr);
+
+    defmt::info!("Hello, world!");
+    panic!("The program stopped");
+}
