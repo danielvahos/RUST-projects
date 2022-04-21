1fddc19daa70712e4a19b388c77f85bbce9b5133
Daniel <daniel.vahosmendoza@telecom-paris.fr>
2022-04-21 19:20:44 +0200

    monotonic working well

--- tp-led-matrix/src/main.rs
+++ tp-led-matrix/src/main.rs
@@ -16,6 +16,9 @@ use tp_led_matrix::image::{Color, Image, RED, GREEN, BLUE};
 use tp_led_matrix::matrix::Matrix;
 use tp_led_matrix::image::Default;
 
+use dwt_systick_monotonic::DwtSystick;
+use dwt_systick_monotonic::ExtU32;
+
 
 /*
 #[panic_handler]
@@ -45,20 +48,24 @@ fn main() -> ! {
 mod app {
     use super::*; //to be able to access Color, Image and so on from RTIC app
 
+    #[monotonic(binds = SysTick, default = true)]
+    type MyMonotonic = DwtSystick<80_000_000>;
+    type Instant = <MyMonotonic as rtic::Monotonic>::Instant;
 
     #[shared]
     struct Shared {}
 
     #[local]
     struct Local {
-        matrix: Matrix
+        matrix: Matrix,
+        newimage:Image
     }
 
     #[init]
     fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
         defmt::info!("defmt correctly initialized");
     
-        let _cp = cx.core;
+        let mut cp = cx.core;
         let dp = cx.device;
     
         // Initialize the clocks, hardware and matrix using your existing code
@@ -94,20 +101,71 @@ mod app {
                     &mut gpioc_moder,&mut gpioc_otyper,
                 clocks,);
 
+        //Monotonic timer instance creation
+        let mut mono = DwtSystick::new(&mut cp.DCB, cp.DWT, cp.SYST, 80_000_000);
 
+        //Build image to display
+        let image_1:Image= Image::new_solid(Color{r:0,g:255,b:0});
+        //let image_1:Image=image_1.default(); //one use default for the image
+        display::spawn(mono.now()).unwrap(); //display task  gets spawned after init() terminates
+        
         // Return the resources and the monotonic timer
-        (Shared {}, Local { matrix }, init::Monotonics())
+        (Shared {}, Local { matrix, newimage }, init::Monotonics(mono))
     }
 
 
 
-    #[idle(local = [matrix])]
+    #[idle(local = [count:usize=0])]
     fn idle(cx: idle::Context) -> ! {
+
+        /* 
         // Display an image on the LED matrix in an infinite loop
         let newimage=Image::gradient(BLUE);
         loop{
             cx.local.matrix.display_image(&newimage);
         }
+        */
+        loop{
+            if *cx.local.count %10_000== 0{
+                defmt::info!("HOLA AMIGOS");
+                *cx.local.count = 0;
+            }
+            *cx.local.count = *cx.local.count + 1;
+        }
+    }
+
+    #[task(local = [matrix, newimage, next_line: usize = 0])]
+    fn display(cx: display::Context, at:Instant) {
+        // Display line next_line (cx.local.next_line) of
+        // the image (cx.local.image) on the matrix (cx.local.matrix).
+        // All those are mutable references.
+        let next=1.secs()/(6*80);
+        let mut next_line = 0;
+        //let next_line: &mut usize=cx.local.next_line;
+
+        // Increment next_line up to 7 and wraparound to 0
+        /*
+        next_line +=1;
+        if next_line>7{
+            next_line=0;
+        }
+        display::spawn_at(at, at+next).unwrap();
+        */
+        for i in 0..8{
+            cx.local.matrix.send_row(next_line,cx.local.newimage.row(next_line));
+            next_line += 1;
+            if next_line == 8{
+                next_line = 0;
+            }
+            }
+        display::spawn_at(at, at + next).unwrap();
+        /* 
+        *cx.local.next_line +=1;
+        if *cx.local.next_line>7{
+            *cx.local.next_line=0;
+        }
+        display::spawn_at(at, at+next).unwrap();
+        */
     }
 
 
